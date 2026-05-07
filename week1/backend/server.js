3c70c6aac09162822fcb92a451ebacbede84faab
Marcellous11 <1mcj001@gmail.com>
2026-05-07 02:35:44 -0500

    production port

--- week1/backend/server.js
+++ week1/backend/server.js
@@ -4,7 +4,7 @@ import { connectMongo,getDB, closeDB } from './db.js';
 import cors from 'cors'
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.use(express.json());
 app.use(cors());
